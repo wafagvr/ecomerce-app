3502ff4e9b0d6664adae1dede2fa1a7394823814
Wafa Ambalathveettil Vell <wafavellaniyil@Wafas-MacBook-Pro.local>
2021-02-07 13:29:53 -0500

    Tachyon css added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <div>Hello Wafa. Happy learning ReactJS</div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello Wafa, Learn ReactJS
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
